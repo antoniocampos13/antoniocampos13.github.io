228512949e146676c2163c9eea7bdfeb5b4b43a5
Antonio Coelho <avccbio@gmail.com>
2020-10-02 17:12:22 -0300

    fix pelicanconf

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,7 +4,7 @@
 AUTHOR = 'Antonio Victor Campos Coelho'
 SITENAME = "Antonio's Portfolio"
 RELATIVE_URLS = False
-SITEURL = 'https://antoniocampos13.github.io/'
+SITEURL = 'https://antoniocampos13.github.io'
 SITETITLE = SITENAME
 SITEDESCRIPTION = "Data Science Portfolio by Antonio Victor Campos Coelho"
 SITESUBTITLE = 'PhD in Genetics'
@@ -32,23 +32,26 @@ TIMEZONE = 'America/Recife'
 DEFAULT_LANG = 'en'
 
 # Feed generation is usually not desired when developing
-FEED_ALL_ATOM = None
-CATEGORY_FEED_ATOM = None
+FEED_DOMAIN = SITEURL
+FEED_ALL_ATOM = 'feeds/all.atom.xml'
+FEED_ALL_RSS = 'feeds/all.rss.xml'
+CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
 TRANSLATION_FEED_ATOM = None
-AUTHOR_FEED_ATOM = None
-AUTHOR_FEED_RSS = None
+AUTHOR_FEED_ATOM = 'feeds/{slug}.atom.xml'
+AUTHOR_FEED_RSS = 'feeds/{slug}.rss.xml'
+RSS_FEED_SUMMARY_ONLY = False
 
 # Blogroll
 LINKS = (
 ('Brazilian Lattes CV', 'http://lattes.cnpq.br/2986394950644755'),
 ('ORCID', 'https://orcid.org/0000-0003-2143-9701'),
-('Researcher ID/Publons', 'https://publons.com/researcher/2414076/antonio-victor-c-coelho/'),
+('Researcher ID/Publons', 'https://publons.com/researcher/2414076/antonio-victor-c-coelho'),
 )
 
 # Social widget
 SOCIAL = (
 ('github', 'https://github.com/antoniocampos13/portfolio'),
-('linkedin', 'https://www.linkedin.com/in/antonio-coelho-9aa338164/'),
+('linkedin', 'https://www.linkedin.com/in/antonio-coelho-9aa338164'),
 )
 
 DEFAULT_PAGINATION = 30
