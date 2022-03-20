7e0379d2fcec7567f73dd2e0894e3726b2a41e9b
Antonio Victor Campos Coelho <86058917+antoniognmk@users.noreply.github.com>
2022-03-20 16:45:26 -0300

    change pelican installation to conda

--- publish_pelican.sh
+++ publish_pelican.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-source /mnt/e/Documents/antonio_github_io/pelican/bin/activate
+#source /mnt/v/Documents/antonio_github_io/pelican/bin/activate
 
 pelican content -o output -s publishconf.py
 ghp-import -m "${1}" --no-jekyll -b master output
